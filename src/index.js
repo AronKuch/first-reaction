04812449bd8fc8ea7580c1e1f01145f8bf2bcd0f
Aron Kuch <AronKuch@gmail.com>
2021-11-02 21:41:57 -0400

    Little test to see if I understand a basic concept.

--- src/index.js
+++ src/index.js
@@ -6,3 +6,8 @@ ReactDOM.render(
   React.createElement("h1", null, "Hello Chucktown"),
   document.getElementById('root')
 );
+
+ReactDOM.render(
+  React.createElement("p", null, "Just a little playing around to see if I understand"),
+  document.getElementById('extra')
+);
