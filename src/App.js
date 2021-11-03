f0275354c38e4af1aa65b375afb1ea2563168ed9
Aron Kuch <AronKuch@gmail.com>
2021-11-03 13:31:33 -0400

    Changes to start using Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+function Head(props){
+  console.log(props);
+  return(
+    <h1>Hello {props.city}, again</h1>
+  );
+}
+
+function Main(){
+  return(
+    <p>Here's new text to share with the world</p>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Head city = "Beantown"/>
+      <Main />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from "./App";
 
 ReactDOM.render(
-  React.createElement(
-    "h1",
-    {style: {color: "#003366"}},
-    "Hello Chucktown"
-  ),
+<App />,
   document.getElementById('root')
-);
-
-ReactDOM.render(
-  React.createElement(
-    "p",
-    null,
-    "Here are some parts of town."
-  ),
-  document.getElementById('extra')
-);
-
-ReactDOM.render(
-  <ul>
-    <li>Downtown</li>
-    <li>West Ashley</li>
-    <li>James Island</li>
-    <li>Park Circle</li>
-  </ul>
-  ,
-  document.getElementById('list')
 );
