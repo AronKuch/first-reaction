d2fa94ce72e8f7929f114d84a3f06f7e62daec17
Aron Kuch <AronKuch@gmail.com>
2021-11-03 20:42:13 -0400

    Added a list, which made things a little dynamic.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,34 @@
 import './App.css';
 
 function Head(props){
-  console.log(props);
   return(
     <h1>Hello {props.city}, again</h1>
   );
 }
 
-function Main(){
+function Main(props){
   return(
-    <p>Here's new text to share with the world</p>
+    <section>
+    <p>Here are some programming languages I have played around with at one point or another.</p>
+    <ul>
+      {props.langs.map((lang) =>
+        (<li key = {lang.id}>{lang.name}</li>))}
+    </ul>
+    </section>
   );
 }
 
+const languages = [
+  "HTML", "CSS", "JavaScript", "Java", "Python", "C", "C++", "R", "MySQL", "PHP", "Apex"
+]
+
+const langObject = languages.map((lang, i) => ({id: i, name: lang}));
 
 function App() {
   return (
     <div className="App">
-      <Head city = "Beantown"/>
-      <Main />
+      <Head city = "Chucktown"/>
+      <Main langs = {langObject} />
     </div>
   );
 }
