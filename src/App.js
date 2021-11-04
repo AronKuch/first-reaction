b5492f9bc92a8d06003316e9e6060ce28f7cd64b
Aron Kuch <AronKuch@gmail.com>
2021-11-03 20:58:21 -0400

    Added pictures.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 
 import './App.css';
+import logo_html5 from "./HTML5.svg";
+import logo_R from "./R.svg";
+import logo_Apex from "./Apex.png";
 
 function Head(props){
   return(
@@ -10,26 +13,33 @@ function Head(props){
 function Main(props){
   return(
     <section>
-    <p>Here are some programming languages I have played around with at one point or another.</p>
+    <p>Here is an abreviated list of programming languages I have played around with at one point or another.</p>
     <ul>
       {props.langs.map((lang) =>
-        (<li key = {lang.id}>{lang.name}</li>))}
+        (<li key = {lang.id}>{lang.name} <img src={lang.logo} alt= {lang.name + " logo"} height = {50} /></li>))}
     </ul>
     </section>
   );
 }
 
-const languages = [
-  "HTML", "CSS", "JavaScript", "Java", "Python", "C", "C++", "R", "MySQL", "PHP", "Apex"
+// const languages = [
+//   "HTML", "CSS", "JavaScript", "Java", "Python", "C", "C++", "R", "MySQL", "PHP", "Apex"
+// ]
+//
+// const langObjects = languages.map((lang, i) => ({id: i, name: lang}));
+
+const langObjects = [
+  {id: 1, name: "HTML5", logo: logo_html5},
+  {id: 0, name: "R", logo: logo_R},
+  {id: 2, name: "Apex", logo: logo_Apex}
 ]
 
-const langObject = languages.map((lang, i) => ({id: i, name: lang}));
 
 function App() {
   return (
     <div className="App">
       <Head city = "Chucktown"/>
-      <Main langs = {langObject} />
+      <Main langs = {langObjects} />
     </div>
   );
 }
