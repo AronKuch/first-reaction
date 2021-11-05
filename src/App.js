59694477d0e248151bdf59991eddc6f36c5e375c
Aron Kuch <AronKuch@gmail.com>
2021-11-04 21:13:27 -0400

    Simple component with conditional rendering

--- src/App.js
+++ src/App.js
@@ -10,36 +10,46 @@ function Head(props){
   );
 }
 
-function Main(props){
+function Pictures(){
+  const langObjects = [
+    {id: 1, name: "HTML5", logo: logo_html5},
+    {id: 0, name: "R", logo: logo_R},
+    {id: 2, name: "Apex", logo: logo_Apex}
+  ]
+
   return(
     <section>
     <p>Here is an abreviated list of programming languages I have played around with at one point or another.</p>
     <ul>
-      {props.langs.map((lang) =>
+      {langObjects.map((lang) =>
         (<li key = {lang.id}>{lang.name} <img src={lang.logo} alt= {lang.name + " logo"} height = {50} /></li>))}
     </ul>
     </section>
   );
 }
 
-// const languages = [
-//   "HTML", "CSS", "JavaScript", "Java", "Python", "C", "C++", "R", "MySQL", "PHP", "Apex"
-// ]
-//
-// const langObjects = languages.map((lang, i) => ({id: i, name: lang}));
+function List(props){
+  const languages = [
+    "HTML", "CSS", "JavaScript", "Java", "Python", "C", "C++", "R", "MySQL", "PHP", "Apex"
+  ]
+  const langListAsObjects = languages.map((lang, i) => ({id: i, name: lang}));
 
-const langObjects = [
-  {id: 1, name: "HTML5", logo: logo_html5},
-  {id: 0, name: "R", logo: logo_R},
-  {id: 2, name: "Apex", logo: logo_Apex}
-]
+  return(
+    <section>
+     <p>Here are some programming languages I have played around with at one point or another.</p>
+     <ul>
+       {langListAsObjects.map((lang) =>
+         (<li key = {lang.id}>{lang.name}</li>))}
+     </ul>
+     </section>
+   );
+}
 
-
-function App() {
+function App(props) {
   return (
     <div className="App">
       <Head city = "Chucktown"/>
-      <Main langs = {langObjects} />
+      {props.pictures ? <Pictures /> : <List />}
     </div>
   );
 }
